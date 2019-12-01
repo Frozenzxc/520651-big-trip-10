29cf847de16c0b6d9b0b37955930af9f9381805c
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2019-12-01 17:13:29 +0300

    Исправляет ошибки

--- src/components/card.js
+++ src/components/card.js
@@ -15,26 +15,25 @@ const createOfferMarkup = (offers) => {
 };
 
 const createCardTemplate = (card) => {
-  const {type, destination, startTime, endTime, offers} = card;
   let sum = 0;
-  offers.forEach((it) => {
+  card.offers.forEach((it) => {
     sum += it.price;
   });
-  const offerList = createOfferMarkup(Array.from(offers));
+  const offerList = createOfferMarkup(Array.from(card.offers));
   return (
     `<li class="trip-events__item">
         <div class="event">
           <div class="event__type">
-            <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+            <img class="event__type-icon" width="42" height="42" src="img/icons/${card.type}.png" alt="Event type icon">
           </div>
-          <h3 class="event__title">${type} to ${destination}</h3>
+          <h3 class="event__title">${card.type} to ${card.destination}</h3>
           <div class="event__schedule">
             <p class="event__time">
-              <time class="event__start-time" datetime="2019-03-18T10:30">${formatTime(startTime)}</time>
+              <time class="event__start-time" datetime="${card.startTime}">${formatTime(card.startTime)}</time>
               &mdash;
-              <time class="event__end-time" datetime="2019-03-18T11:00">${formatTime(endTime)}</time>
+              <time class="event__end-time" datetime="${card.endTime}">${formatTime(card.endTime)}</time>
             </p>
-            <p class="event__duration">${getDuration(startTime, endTime)}</p>
+            <p class="event__duration">${getDuration(card.startTime, card.endTime)}</p>
           </div>
           <p class="event__price">
             &euro;&nbsp;<span class="event__price-value">${sum}</span>
